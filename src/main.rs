6e58debe759603a593185ff3ff3b7d34d3a32e13
erickloeckner <48753426+erickloeckner@users.noreply.github.com>
2025-03-09 03:35:27 -0400

    Create main.rs

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use std::env;
+use std::io::prelude::*;
+    use std::net::TcpStream;
+
+fn main() -> std::io::Result<()> {
+    let location = env::args()
+        .nth(1)
+        .unwrap_or("".to_string());
+    
+    let command_string = env::args()
+        .nth(2)
+        .unwrap_or("0".to_string());
+    let command = u8::from_str_radix(&command_string, 10).unwrap_or(0);
+
+    let value_string = env::args()
+        .nth(3)
+        .unwrap_or("0".to_string());
+    let value = u8::from_str_radix(&value_string, 10).unwrap_or(0);
+
+    let message = [0, 0, 0, command, value, 255, 255, 255];
+    let mut stream = TcpStream::connect(&location)?;
+    stream.write(&message)?;
+
+    Ok(())
+}
